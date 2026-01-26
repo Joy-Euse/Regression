3d64b12b941061cfe3e13975840e14a833148b4c
Joyeuse <iradukundajoyeuse34@example.com>
2026-01-26 09:52:46 +0200

    Initial commit

---
+++ performance/train_model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+
+def train():
+    df = pd.read_csv('C:/Users/Public/COURSES/Year3/Machine Learning/work/StudentPerformance.csv')
+    df['Extracurricular Activities'] = LabelEncoder().fit_transform(
+        df['Extracurricular Activities']
+        )
+    
+    X = df.drop('Performance Index', axis=1)
+    y = df['Performance Index']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+        )
+    
+    model = RandomForestRegressor(n_estimators=200, random_state=42)
+    model.fit(X_train, y_train)
+    
+    joblib.dump(model, 'performance/model.pkl')
+    print("Model trained and saved")
